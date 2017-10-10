d06dcc7f87249783a20b1229fea078f67f17607c
Daniel Muñoz Martín <27631697+danimm@users.noreply.github.com>
2017-10-10 18:07:28 +0200

    Subiendo el proyecto

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appname: 'appmusic',
+  apiKey: 'a53a266a5c2a163e2100c56f612d117f',
+  secret: 'aa7c66a80bdc1990c2900a30ff417c60',
+  registeredTo: 'danimmdev'
+}
+
+// Application name	appmusic
+// API key	a53a266a5c2a163e2100c56f612d117f
+// Shared secret	aa7c66a80bdc1990c2900a30ff417c60
+// Registered to	danimmdev
